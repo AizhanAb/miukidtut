8cef1823e165196b0bb2d2c92170cdd2edff6813
Aizhan Abdraiymova <61827245+AizhanAb@users.noreply.github.com>
2021-10-07 20:59:47 -0500

    Add files via upload
    urok 29

---
+++ addtocart.js
@@ -0,0 +1,11 @@
+let price=document.getElementById("shoppingicon").addEventListener('click', function(e){
+  let text = document.getElementById("price").innerHTML
+  console.log(text);
+  let texttitle = document.getElementById("title1").innerHTML
+  console.log(texttitle)
+  let textimg = document.getElementById("img1").alt
+    console.log(textimg)
+});
+``
+
+
